378ebde2124db6848f5c7bbc6b5bfc91061cbd45
AstronomikalOne <livewire_Static@hotmail.com>
2026-02-19 12:40:42 -0800

    Remove synrix-windows-build from repo; this repo is for synrix-sdks only

--- synrix-windows-build/build/windows/include/license_global.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef SYNRIX_LICENSE_GLOBAL_H
-#define SYNRIX_LICENSE_GLOBAL_H
-
-#include <stdint.h>
-
-int license_global_register(uint32_t node_count, uint32_t limit);
-int license_global_add_one(uint32_t limit);
-
-#endif
--- synrix-windows-build/python-sdk/examples/activate_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Activate Python environment on NVME
-source /mnt/nvme/aion-omega/python-env/bin/activate
-echo "✓ Python environment activated (on NVME)"
-echo "  Python: $(which python3)"
-echo "  Location: /mnt/nvme/aion-omega/python-env"
